eefe1f43e1719bbcb721894909d4a42014d6f965
Yaron Dinur <prsnca@gmail.com>
2020-04-12 22:49:56 +0300

    added lang field

--- reminders/models.py
+++ reminders/models.py
@@ -5,4 +5,5 @@ from django.utils import timezone
 class Reminder(models.Model):
     chat_id = models.CharField(max_length=64, unique=True, db_index=True)
     hour = models.IntegerField(default=18, db_index=True)
+    lang = models.CharField(max_length=3, default='he')
     created_at = models.DateTimeField(default=timezone.now)
